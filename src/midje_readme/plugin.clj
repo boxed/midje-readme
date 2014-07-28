bf10954b56c013ebe4f6caec6e9ecd275a3552ea
Anders Hovmöller <boxed@killingar.net>
2014-07-28 17:03:44 +0200

    Remove readme.clj if running older versions of clojure

--- src/midje_readme/plugin.clj
+++ src/midje_readme/plugin.clj
@@ -1,4 +1,5 @@
-(ns midje-readme.plugin)
+(ns midje-readme.plugin
+  (:require [leiningen.core.eval :refer :all]))
 
 (defn readme-to-midje-test [readme require-str]
   (let [keep-line (atom false)
@@ -19,9 +20,15 @@
                                 0 (format "(ns readme (:use midje.sweet) (:require %s)) (def ... :...)" require-str)))))
 
 (defn middleware [project]
-  (spit "test/readme.clj"
-        (readme-to-midje-test (slurp "README.md")
-                              (or (get-in project [:midje-readme :require])
-                                  (format "[%s.core :refer :all]" (:group project)))))
+  (let [used-clojure-version (read-string (with-out-str (eval-in project '(prn *clojure-version*))))]
+    (if (or (> (:major used-clojure-version) 1)
+            (> (:minor used-clojure-version) 3))
+      (spit "test/readme.clj"
+            (readme-to-midje-test (slurp "README.md")
+                                  (or (get-in project [:midje-readme :require])
+                                      (format "[%s.core :refer :all]" (:group project)))))
+      (do
+        (clojure.java.io/delete-file "test/readme.clj" true)
+        (println "Warning: midje-readme doesn't support clojure < 1.4. The readme will not be tested"))))
 
   project)
